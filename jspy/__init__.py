c63133900663e233c24c1afb78a727937f1d91ba
Marek Stępniowski <marek@stepniowski.com>
2012-04-17 16:54:30 +0200

    Extract `jspy.eval_string` function from `jspy.eval_file`.

--- jspy/__init__.py
+++ jspy/__init__.py
@@ -10,20 +10,22 @@ def create_default_global_objects():
     return {'console': Console()}
 
 
-def eval_file(file_name, global_objects=None):
+def eval_string(s, global_objects=None):
     if global_objects is None:
         global_objects = create_default_global_objects()
-
-    # Parse file
-    f = codecs.open(file_name, encoding='utf-8')
-    file_contents = f.read()
-    program = Parser().parse(file_contents)
+        
+    # Parse file and create the execution context object
+    program = Parser().parse(s)
     declared_vars = dict((name, UNDEFINED) for name in program.get_declared_vars())
     declared_vars.update(global_objects)
-
-    # Create the execution context object
     context = ExecutionContext(declared_vars)
 
     # Run code
     result = program.eval(context)
     return result.value, context
+
+
+def eval_file(file_name, global_objects=None):
+    f = codecs.open(file_name, encoding='utf-8')
+    file_contents = f.read()
+    return eval_string(file_contents)
